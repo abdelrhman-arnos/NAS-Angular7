b24599a6abedfe3411ffa79066ae46e63e67a363
Abdelrhman Mohamed <abdelrhman@codersea.com>
2019-03-02 10:52:27 +0200

    Components, libs, UI
    - Built Home page with 2 components(Services, Slider).
    - Built shared Header.
    - Added Bootstrap 4, GlideJS.
    - Added new assets(Styles, Fonts, SVGs, Imgs).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderModule} from './shared/header/header.module';
+import {SliderModule} from './home/slider/slider.module';
+import {ServicesModule} from './home/services/services.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    SliderModule,
+    ServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
---
+++ src/app/home/services/services.component.ts
@@ -0,0 +1,49 @@
+import {Component, AfterViewInit} from '@angular/core';
+import Glide from 'node_modules/@glidejs/glide/dist/glide.min';
+
+declare function require(path: string);
+const newIcon = require('../../../assets/svg/Services-New-Icon.svg');
+const allIcon = require('../../../assets/svg/Services-All-Icon.svg');
+const mostUsedIcon = require('../../../assets/svg/Services-Most-Used-Icon.svg');
+const individualsIcon = require('../../../assets/svg/Services-Individuals-Icon.svg');
+const investorsIcon = require('../../../assets/img/Services-Investors-Icon.png');
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.sass']
+})
+
+export class ServicesComponent implements AfterViewInit{
+  serviceActive = 1;
+  services: object[] = [
+    {
+      name: 'خدمات جديدة',
+      img: newIcon
+    },
+    {
+      name: 'جميع الخدمات',
+      img: allIcon
+    },
+    {
+      name: 'الخدمات الأكثر إستخداما',
+      img: mostUsedIcon
+    },
+    {
+      name: 'خدمات الأفراد',
+      img: individualsIcon
+    },
+    {
+      name: 'خدمات قطاع الأعمال',
+      img: investorsIcon
+    }
+  ];
+  ngAfterViewInit() {
+    new Glide('#services', {
+      autoplay: 3000,
+      startAt: 2,
+      perView: Math.round(window.innerWidth * .4 / 100),
+      bound: true
+    }).mount();
+  }
+}
